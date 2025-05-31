d4ff96e0ba73e3c4e91b64d03a66baa206ee1a61
Matheus <mbazarello@gmail.com>
2025-05-31 18:05:52 -0300

    .

--- backend/start.js
+++ backend/start.js
@@ -1,11 +1,8 @@
-require('dotenv').config({ path: 'api.env' }); 
-
+require('dotenv').config({ path: 'api.env' }); // localmente, carrega o api.env
 const express = require('express');
-const path = require('path'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Rota da API
 app.get('/api/chave-google', (req, res) => {
   const apiKey = process.env.GOOGLE_API_KEY;
   if (!apiKey) {
@@ -14,14 +11,7 @@ app.get('/api/chave-google', (req, res) => {
   res.json({ apiKey });
 });
 
-
-const frontendPath = path.join(__dirname, '../frontend/public'); 
-app.use(express.static(frontendPath));
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(frontendPath, 'index.html'));
-});
+app.use(express.static('../frontend/public'));
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
