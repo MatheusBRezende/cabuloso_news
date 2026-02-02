7a872eacaecc629e4c8cb2bcd2c0aa11ca8871fd
MatheusBRezende <mbazarello@gmail.com>
2026-02-01 23:32:42 -0300

    .

--- script/live-match-detector.js
+++ script/live-match-detector.js
@@ -1,7 +1,7 @@
 
 const LiveMatchDetector = (() => {
   const CONFIG = {
-    webhookUrl: 'https://directions-bali-cannon-change.trycloudflare.com/webhook/placar-ao-vivo',
+    webhookUrl: 'https://spikeofino-meu-n8n-cabuloso.hf.space/webhook/placar-ao-vivo',
     checkInterval: 5000, 
     minutoAMinutoUrl: '../minuto-a-minuto.html', 
     countdownSeconds: 10,
--- script/minuto-a-minuto.js
+++ script/minuto-a-minuto.js
@@ -4,8 +4,7 @@
  */
 
 const CONFIG = {
-  webhookUrl:
-    "https://directions-bali-cannon-change.trycloudflare.com/webhook/placar-ao-vivo",
+  webhookUrl:"https://spikeofino-meu-n8n-cabuloso.hf.space/webhook/placar-ao-vivo",
   agendaUrl: "./backend/agenda_cruzeiro.json",
   updateInterval: 10000,
 };
